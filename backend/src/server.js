bfbdb071a24ecb7ae05df6d67a487710c3c0b35b
Aditya Gupta <adityagupta7256@gmail.com>
2026-05-24 13:58:14 +0530

    Add: protected route middleware created

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 
+
 dotenv.config();
 
 connectDB();
@@ -13,6 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 
